3c5b8bd3e2f61a35c0d805c948c48598ad88f187
mukund0502 <84224791+mukund0502@users.noreply.github.com>
2022-07-03 14:13:40 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+var todo = document.querySelector("input");
+var button = document.querySelector("button");
+var list = document.getElementsByClassName("work");
+var delBtn = document.getElementsByClassName("deletebutton");
+
+
+
+button.addEventListener('click', add);
+
+function add(){
+    for (let index = 0; index < 10; index++) {
+        if(list[index].innerHTML == ""){
+            list[index].style.display = "block";
+            list[index].style.visibility = "visible";
+            list[index].innerHTML = todo.value;
+            delBtn[index].style.visibility = "visible";
+            break;
+        }
+    }
+
+}
+
+for (let index = 0; index < 10; index++) {
+    delBtn[index].addEventListener('click', () => {
+        list[index].innerHTML = "";
+        delBtn[index].style.visibility = "hidden";
+        list[index].style.display = "none";
+    });
+}
